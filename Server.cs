71451da93c4a12ad682e4c505d444a71612736a8
NivZiv <65464187+NivZiv@users.noreply.github.com>
2020-05-16 21:44:38 +0300

    Server
    The code behind the Server-side

---
+++ Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+
+namespace ServerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trigger the method PrintIncomingMessage when a packet of type 'Message' is received
+            //We expect the incoming object to be a string which we state explicitly by using <string>
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", PrintIncomingMessage);
+            //Start listening for incoming connections
+            Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0));
+
+            //Printing the server information for connecting purpose
+            Console.WriteLine("Server listening for TCP connection on:");
+            foreach (System.Net.IPEndPoint localEndPoint in Connection.ExistingLocalListenEndPoints(ConnectionType.TCP))
+                Console.WriteLine("{0}:{1}", localEndPoint.Address, localEndPoint.Port);
+
+            //Letting the server an option to close the connect, next line, reading the key.
+            Console.WriteLine("\nPress any key to close server.");
+            Console.ReadKey(true);
+
+            //'NetworkComms.Shutdown()', to ensure we closed the connection
+            NetworkComms.Shutdown();
+        }
+
+        //Creating a class for the message
+        private static void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
+        {
+            Console.WriteLine("\nMessage received from " + connection.ToString() + " - '" + message + "'.");
+        }
+    }
+}
